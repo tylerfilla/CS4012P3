ad30c8e19e295dfa8c795c15ac4fe886147a9db8
Tyler Filla <tylerfilla@hotmail.com>
2018-11-25 14:10:19 -0600

    Add service classes

---
+++ src/main/java/edu/umsl/mail/tsfn88/project3/context/api/site/service/UserService.java
@@ -0,0 +1,4 @@
+package edu.umsl.mail.tsfn88.project3.context.api.site.service;
+
+public interface UserService {
+}
---
+++ src/main/java/edu/umsl/mail/tsfn88/project3/context/api/site/service/impl/DefaultUserService.java
@@ -0,0 +1,8 @@
+package edu.umsl.mail.tsfn88.project3.context.api.site.service.impl;
+
+import edu.umsl.mail.tsfn88.project3.context.api.site.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DefaultUserService implements UserService {
+}
